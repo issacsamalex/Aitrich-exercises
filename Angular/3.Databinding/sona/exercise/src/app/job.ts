a46c1bfccf060d8b5e7aa29e92877907b90add43
divyasushinanay <divyasushin.aitrich@gmail.com>
2025-04-04 17:17:25 +0530

    angularadded

--- Angular/3.Databinding/sona/exercise/src/app/job.ts
+++
@@ -1,9 +0,0 @@
-export class job{
-    id:string|undefined;
-    dateapplied:string|undefined;
-    company:string|undefined;
-    type:string|undefined;
-    position:string|undefined;
-    contact:string|undefined;
-    status:string|undefined;
-}
